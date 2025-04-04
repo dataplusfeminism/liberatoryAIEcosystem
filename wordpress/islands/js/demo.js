bed27a57621efbeea25b7fc2d4ee05faf2415351
Bianchi Dy <bianchi@kontinentalist.com>
2025-04-04 12:17:07 -0400

    feat: fix authors page and headers

--- wordpress/islands/js/demo.js
+++ wordpress/islands/js/demo.js
@@ -1,5 +1,22 @@
 (function() {
     // alert('javascript loaded.');
+    // Nav gradient cursor
+    const element = document.querySelector('.wp-block-template-part');
+
+    element.addEventListener('mousemove', (e) => {
+    const rect = element.getBoundingClientRect();
+    const x = ((e.clientX - rect.left) / rect.width) * 100;
+    const y = ((e.clientY - rect.top) / rect.height) * 100;
+
+    element.style.setProperty('--x', `${x}%`);
+    element.style.setProperty('--y', `${y}%`);
+    });
+
+    element.addEventListener('mouseleave', () => {
+    // Optional: Reset to center or freeze where it was
+    element.style.setProperty('--x', `50%`);
+    element.style.setProperty('--y', `50%`);
+    });
     
 })();
 
