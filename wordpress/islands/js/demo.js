7dda20b9f2920b7fb7b3053ccd678d66a5754576
Bianchi Dy <bianchi@kontinentalist.com>
2025-04-04 13:19:09 -0400

    feat: update nav and footer cursor

--- wordpress/islands/js/demo.js
+++ wordpress/islands/js/demo.js
@@ -1,21 +1,23 @@
 (function() {
     // alert('javascript loaded.');
     // Nav gradient cursor
-    const element = document.querySelector('.wp-block-template-part');
+    const elements = document.querySelectorAll('.wp-block-template-part');
 
-    element.addEventListener('mousemove', (e) => {
-    const rect = element.getBoundingClientRect();
-    const x = ((e.clientX - rect.left) / rect.width) * 100;
-    const y = ((e.clientY - rect.top) / rect.height) * 100;
-
-    element.style.setProperty('--x', `${x}%`);
-    element.style.setProperty('--y', `${y}%`);
-    });
-
-    element.addEventListener('mouseleave', () => {
-    // Optional: Reset to center or freeze where it was
-    element.style.setProperty('--x', `50%`);
-    element.style.setProperty('--y', `50%`);
+    elements.forEach(element => {
+      element.addEventListener('mousemove', (e) => {
+        const rect = element.getBoundingClientRect();
+        const x = ((e.clientX - rect.left) / rect.width) * 100;
+        const y = ((e.clientY - rect.top) / rect.height) * 100;
+    
+        element.style.setProperty('--x', `${x}%`);
+        element.style.setProperty('--y', `${y}%`);
+      });
+    
+      element.addEventListener('mouseleave', () => {
+        // Reset to center when mouse leaves
+        element.style.setProperty('--x', `50%`);
+        element.style.setProperty('--y', `50%`);
+      });
     });
     
     // Get URL of class for specific targetting
